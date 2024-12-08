888b7771b1b843a5b47ec63451685be78336c403
Guilherme <guilhermepontifice.gp@gmail.com>
2024-12-08 10:30:28 -0300

    trabalhos

--- src/routes/Trabalho.jsx
+++ src/routes/Trabalho.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
 function Trabalho() {
   return (
-    <div>Trabalho</div>
-  )
+    <div className="trabalho">
+      <div className="cards">
+        <div className="card">
+          <h1>Desenvolvedor Front-End</h1>
+          <h2>Yaslip</h2>
+          <h3>Jun/24 - Atualmente</h3>
+          <p>
+            Como desenvolvedor na Yaslip, meu dia a dia consiste em aumentar a
+            performance de páginas, aumentando também a eficiência, criar
+            Layouts responsivos que estejam de acordo com as demandas dos
+            clientes, participando inclusive de reuniões de alinhamento com
+            eles. Além disso, tenho que fazer análise do ranqueamento de sites
+            com otimização para mecanismos de busca, de forma orgânica. Meu dia
+            a dia inclui: • JavaScript • PHP • SCSS • Google Analytics • Excel •
+            GIT • React • Sass • UX/UI
+          </p>
+          <div className="techs"></div>
+        </div>
+        <div className="card">
+          <h1>Desenvolvedor Front-End</h1>
+          <h2>Estudando</h2>
+          <h3>Jan/23 - Maio/24</h3>
+          <p>
+            Como engenheiro de software, meu foco é criar e manter sistemas
+            eficientes, utilizando JavaScript, React e TypeScript. Desenvolvo
+            interfaces web com NodeJS no backend, otimizando para velocidade e
+            desempenho. Implemento práticas de segurança, adoto Desenvolvimento
+            Orientado a Testes com Jest e utilizo Git para um versionamento
+            eficiente. Minha abordagem visa garantir códigos limpos, seguros e
+            de fácil manutenção, proporcionando a melhor experiência ao usuário
+          </p>
+          <div className="techs"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Trabalho
+export default Trabalho;
